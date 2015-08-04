294a9a68c0f0e5bcbd0166e54c039a982620f3a2
ljavier <lorenzo.javier@one.verizon.com>
2015-08-03 17:08:33 -0700

    Update 08/03/2015

--- sw_fare_reducer.py
+++ sw_fare_reducer.py
@@ -14,6 +14,7 @@ from htmlentitydefs import name2codepoint
 # http://www.blog.pythonlibrary.org/2012/06/08/python-101-how-to-submit-a-web-form/
 # http://www.thetaranights.com/fill-online-form-using-python/
 # http://wwwsearch.sourceforge.net/mechanize/download.html
+# http://readwrite.com/2014/06/27/raspberry-pi-web-server-website-hosting
 global temp
 global outboundTime
 global returnTime
@@ -147,125 +148,179 @@ cwd = os.getcwd()
 resultsFile = cwd+"/southwest_results.html"
 responseFile = cwd+"/southwest_response.html"
 print ""
-for x in range(0,len(upcoming_trips)):
-	confirmationNum = upcoming_trips[x][0]
-	firstName = upcoming_trips[x][1]
-	lastName = upcoming_trips[x][2]
-	notificationAddress = upcoming_trips[x][3]
-	originAirportCode = upcoming_trips[x][4]
-	for i in range(0,len(airport_list)):
-		if originAirportCode in airport_list[i][0]:
-			originAirportName = airport_list[i][1]
-			break
-	destinationAirportCode = upcoming_trips[x][5]
-	for i in range(0,len(airport_list)):
-		if destinationAirportCode in airport_list[i][0]:
-			destinationAirportName = airport_list[i][1]
-			break
-	outboundDate = upcoming_trips[x][6]
-	temp = datetime.datetime.strptime(outboundDate, "%m/%d/%Y")
-	outboundDay = temp.strftime("%A")
-	outboundDepartTime = upcoming_trips[x][7]
-	temp = time.strptime(outboundDepartTime, "%I:%M%p")
-	outboundDepartTime24Hour = float(temp.tm_hour) + float(float(temp.tm_min) / 60)
-	outboundArriveTime = upcoming_trips[x][8]
-	outboundFlightNum = upcoming_trips[x][9]
-	paidDollarsPrice = upcoming_trips[x][10]
-	paidPointsPrice = upcoming_trips[x][11]
 
-	br = mechanize.Browser()
-	br.set_handle_robots(False)
-	response = br.open("https://www.southwest.com/flight/")
-	content = response.read()
-	with open(responseFile, "w") as f:
-	    f.write(content)
-	br.select_form(name="buildItineraryForm")
-	# br.select_form(nr=2)
-	br.find_control(name="originAirport").value = [originAirportCode]
-	br.find_control(name="destinationAirport").value = [destinationAirportCode]
-	br.form["outboundDateString"] = outboundDate
-	if(outboundDepartTime24Hour < 12):
-		br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['BEFORE_NOON']
-	elif(18 >= outboundDepartTime24Hour <= 12):
-		br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['NOON_TO_6PM']
-	elif(outboundDepartTime24Hour > 18):
-		br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['AFTER_6PM']
-	br.find_control(id="roundTrip",name="twoWayTrip").value = ['false']
-	try:
-		br.find_control(name="fareType").value = ['POINTS']
-	except:
-		print "WARNING: Fare type selection is not accessible at the moment.\n"
-	try:
-		result = br.submit()
-	except:
-		print "ERROR: Could not submit information "
-		continue
-	southwest_results_string = result.read()
-	with open(resultsFile, "w") as f:
-	    f.write(southwest_results_string)
+db = MySQLdb.connect("localhost","root","swfarereducer","SWFAREREDUCERDB")
+cursor = db.cursor()
+sql = "SELECT * FROM SWFAREREDUCERDB.UPCOMING_FLIGHTS"
+try:
+	cursor.execute(sql)
+	results = cursor.fetchall()
+	for row in results:
+		confirmationNum = row[1]
+		firstName = row[2]
+		lastName = row[3]
+		notificationAddress = row[4]
+		originAirportCode = row[5]
+		for i in range(0,len(airport_list)):
+			if originAirportCode in airport_list[i][0]:
+				originAirportName = airport_list[i][1]
+				break
+		destinationAirportCode = row[6]
+		for i in range(0,len(airport_list)):
+			if destinationAirportCode in airport_list[i][0]:
+				destinationAirportName = airport_list[i][1]
+				break
+		outboundDate = row[7]
+		temp = datetime.datetime.strptime(outboundDate, "%m/%d/%Y")
+		outboundDay = temp.strftime("%A")
+		outboundDepartTime = row[8]
+		temp = time.strptime(outboundDepartTime, "%I:%M%p")
+		outboundDepartTime24Hour = float(temp.tm_hour) + float(float(temp.tm_min) / 60)
+		outboundArriveTime = row[9]
+		outboundFlightNum = row[10]
+		paidDollarsPrice = row[11]
+		paidPointsPrice = row[12]
 
-	parser = MyHTMLParser()
+		br = mechanize.Browser()
+		br.set_handle_robots(False)
+		response = br.open("https://www.southwest.com/flight/")
+		content = response.read()
+		with open(responseFile, "w") as f:
+		    f.write(content)
+		br.select_form(name="buildItineraryForm")
+		# br.select_form(nr=2)
+		br.find_control(name="originAirport").value = [originAirportCode]
+		br.find_control(name="destinationAirport").value = [destinationAirportCode]
+		br.form["outboundDateString"] = outboundDate
+		if(outboundDepartTime24Hour < 12):
+			br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['BEFORE_NOON']
+		elif(18 >= outboundDepartTime24Hour <= 12):
+			br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['NOON_TO_6PM']
+		elif(outboundDepartTime24Hour > 18):
+			br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['AFTER_6PM']
+		br.find_control(id="roundTrip",name="twoWayTrip").value = ['false']
+		try:
+			br.find_control(name="fareType").value = ['POINTS']
+		except:
+			print "WARNING: Fare type selection is not accessible at the moment.\n"
+		try:
+			result = br.submit()
+		except:
+			print "ERROR: Could not submit information "
+			continue
+		southwest_results_string = result.read()
+		with open(resultsFile, "w") as f:
+		    f.write(southwest_results_string)
 
-	print originAirportName+" ---> "+destinationAirportName+" [ "+outboundDay+", "+outboundDate+" ]"
-	for x in range(1,30):
-		inputPosBeg = southwest_results_string.find("<input id=\"Out"+str(x)+"C\"")
-		if(inputPosBeg != -1):
-			inputPosEnd = southwest_results_string.find("/>", inputPosBeg)
-			outboundFlightResult = southwest_results_string[(inputPosBeg):(inputPosEnd+2)]
-			parser.feed(outboundFlightResult)
+		parser = MyHTMLParser()
 
-			if( (flightNum == outboundFlightNum) ):
-				inputPosBeg2 = southwest_results_string.find("<label", inputPosBeg)
-				inputPosEnd2 = southwest_results_string.find("</label>", inputPosBeg2)
-				outboundFlightResult = southwest_results_string[(inputPosBeg2):(inputPosEnd2+8)]
+		print originAirportName+" ---> "+destinationAirportName+" [ "+outboundDay+", "+outboundDate+" ]"
+		for x in range(1,30):
+			inputPosBeg = southwest_results_string.find("<input id=\"Out"+str(x)+"C\"")
+			if(inputPosBeg != -1):
+				inputPosEnd = southwest_results_string.find("/>", inputPosBeg)
+				outboundFlightResult = southwest_results_string[(inputPosBeg):(inputPosEnd+2)]
 				parser.feed(outboundFlightResult)
 
-				print "%s (%s)\t%s\t%s\t%s\t%s\t(Flight # %s)\t%s\n" % (currentDollarsPrice,currentPointsPrice,departTime,departTag,arriveTime,arriveTag,flightNum,route)
+				if( (flightNum == outboundFlightNum) ):
+					inputPosBeg2 = southwest_results_string.find("<label", inputPosBeg)
+					inputPosEnd2 = southwest_results_string.find("</label>", inputPosBeg2)
+					outboundFlightResult = southwest_results_string[(inputPosBeg2):(inputPosEnd2+8)]
+					parser.feed(outboundFlightResult)
+
+					print "%s (%s)\t%s\t%s\t%s\t%s\t(Flight # %s)\t%s\n" % (currentDollarsPrice,currentPointsPrice,departTime,departTag,arriveTime,arriveTag,flightNum,route)
+					
+					if( float(currentDollarsPrice.replace('$','')) < float(paidDollarsPrice) ):
+						send_alert(notificationAddress,"$"+paidDollarsPrice,currentDollarsPrice,confirmationNum,originAirportCode,destinationAirportCode,outboundDate,outboundFlightNum)
+					elif( float(currentPointsPrice) < float(paidPointsPrice) ):
+						send_alert(notificationAddress,paidPointsPrice,currentPointsPrice,confirmationNum,originAirportCode,destinationAirportCode,outboundDate,outboundFlightNum)
+					break
+except:
+	print "ERROR: Unable to fetch data"
+	sys.exit()
+
+db.close()
+
+# for x in range(0,len(upcoming_trips)):
+# 	confirmationNum = upcoming_trips[x][0]
+# 	firstName = upcoming_trips[x][1]
+# 	lastName = upcoming_trips[x][2]
+# 	notificationAddress = upcoming_trips[x][3]
+# 	originAirportCode = upcoming_trips[x][4]
+# 	for i in range(0,len(airport_list)):
+# 		if originAirportCode in airport_list[i][0]:
+# 			originAirportName = airport_list[i][1]
+# 			break
+# 	destinationAirportCode = upcoming_trips[x][5]
+# 	for i in range(0,len(airport_list)):
+# 		if destinationAirportCode in airport_list[i][0]:
+# 			destinationAirportName = airport_list[i][1]
+# 			break
+# 	outboundDate = upcoming_trips[x][6]
+# 	temp = datetime.datetime.strptime(outboundDate, "%m/%d/%Y")
+# 	outboundDay = temp.strftime("%A")
+# 	outboundDepartTime = upcoming_trips[x][7]
+# 	temp = time.strptime(outboundDepartTime, "%I:%M%p")
+# 	outboundDepartTime24Hour = float(temp.tm_hour) + float(float(temp.tm_min) / 60)
+# 	outboundArriveTime = upcoming_trips[x][8]
+# 	outboundFlightNum = upcoming_trips[x][9]
+# 	paidDollarsPrice = upcoming_trips[x][10]
+# 	paidPointsPrice = upcoming_trips[x][11]
+
+# 	br = mechanize.Browser()
+# 	br.set_handle_robots(False)
+# 	response = br.open("https://www.southwest.com/flight/")
+# 	content = response.read()
+# 	with open(responseFile, "w") as f:
+# 	    f.write(content)
+# 	br.select_form(name="buildItineraryForm")
+# 	# br.select_form(nr=2)
+# 	br.find_control(name="originAirport").value = [originAirportCode]
+# 	br.find_control(name="destinationAirport").value = [destinationAirportCode]
+# 	br.form["outboundDateString"] = outboundDate
+# 	if(outboundDepartTime24Hour < 12):
+# 		br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['BEFORE_NOON']
+# 	elif(18 >= outboundDepartTime24Hour <= 12):
+# 		br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['NOON_TO_6PM']
+# 	elif(outboundDepartTime24Hour > 18):
+# 		br.find_control(id="outboundTimeOfDay",name="outboundTimeOfDay").value = ['AFTER_6PM']
+# 	br.find_control(id="roundTrip",name="twoWayTrip").value = ['false']
+# 	try:
+# 		br.find_control(name="fareType").value = ['POINTS']
+# 	except:
+# 		print "WARNING: Fare type selection is not accessible at the moment.\n"
+# 	try:
+# 		result = br.submit()
+# 	except:
+# 		print "ERROR: Could not submit information "
+# 		continue
+# 	southwest_results_string = result.read()
+# 	with open(resultsFile, "w") as f:
+# 	    f.write(southwest_results_string)
+
+# 	parser = MyHTMLParser()
+
+# 	print originAirportName+" ---> "+destinationAirportName+" [ "+outboundDay+", "+outboundDate+" ]"
+# 	for x in range(1,30):
+# 		inputPosBeg = southwest_results_string.find("<input id=\"Out"+str(x)+"C\"")
+# 		if(inputPosBeg != -1):
+# 			inputPosEnd = southwest_results_string.find("/>", inputPosBeg)
+# 			outboundFlightResult = southwest_results_string[(inputPosBeg):(inputPosEnd+2)]
+# 			parser.feed(outboundFlightResult)
+
+# 			if( (flightNum == outboundFlightNum) ):
+# 				inputPosBeg2 = southwest_results_string.find("<label", inputPosBeg)
+# 				inputPosEnd2 = southwest_results_string.find("</label>", inputPosBeg2)
+# 				outboundFlightResult = southwest_results_string[(inputPosBeg2):(inputPosEnd2+8)]
+# 				parser.feed(outboundFlightResult)
+
+# 				print "%s (%s)\t%s\t%s\t%s\t%s\t(Flight # %s)\t%s\n" % (currentDollarsPrice,currentPointsPrice,departTime,departTag,arriveTime,arriveTag,flightNum,route)
 				
-				if( float(currentDollarsPrice.replace('$','')) < float(paidDollarsPrice) ):
-					send_alert(notificationAddress,"$"+paidDollarsPrice,currentDollarsPrice,confirmationNum,originAirportCode,destinationAirportCode,outboundDate,outboundFlightNum)
-				elif( float(currentPointsPrice) < float(paidPointsPrice) ):
-					send_alert(notificationAddress,paidPointsPrice,currentPointsPrice,confirmationNum,originAirportCode,destinationAirportCode,outboundDate,outboundFlightNum)
-				break
-
-'''
-Alltel
-[10-digit phone number]@message.alltel.com
-Example: 1234567890@message.alltel.com
-
-AT&T (formerly Cingular)
-[10-digit phone number]@txt.att.net
-[10-digit phone number]@mms.att.net (MMS)
-[10-digit phone number]@cingularme.com
-Example: 1234567890@txt.att.net
-
-Nextel (now Sprint Nextel)
-[10-digit telephone number]@messaging.nextel.com
-Example: 1234567890@messaging.nextel.com
-
-Sprint PCS (now Sprint Nextel)
-[10-digit phone number]@messaging.sprintpcs.com
-[10-digit phone number]@pm.sprint.com (MMS)
-Example: 1234567890@messaging.sprintpcs.com
-
-T-Mobile
-[10-digit phone number]@tmomail.net
-Example: 1234567890@tmomail.net
-
-US Cellular
-[10-digit phone number]email.uscc.net (SMS)
-[10-digit phone number]@mms.uscc.net (MMS)
-Example: 1234567890@email.uscc.net
-
-Verizon
-[10-digit phone number]@vtext.com
-[10-digit phone number]@vzwpix.com (MMS)
-Example: 1234567890@vtext.com
-
-Virgin Mobile USA
-[10-digit phone number]@vmobl.com
-Example: 1234567890@vmobl.com
-'''
+# 				if( float(currentDollarsPrice.replace('$','')) < float(paidDollarsPrice) ):
+# 					send_alert(notificationAddress,"$"+paidDollarsPrice,currentDollarsPrice,confirmationNum,originAirportCode,destinationAirportCode,outboundDate,outboundFlightNum)
+# 				elif( float(currentPointsPrice) < float(paidPointsPrice) ):
+# 					send_alert(notificationAddress,paidPointsPrice,currentPointsPrice,confirmationNum,originAirportCode,destinationAirportCode,outboundDate,outboundFlightNum)
+# 				break
 
 '''
 "CAK":{
